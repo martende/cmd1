a74f0c8efab66dcdd90059cc731a9d097320373e
Aleh Biruk <aleh.biruk@ltnglobal.com>
2020-10-16 11:04:21 +0200

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/martende/cmd1
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+import "github.com/martende/mod1"
+
+func main() {
+	fmt.Println("hello worla",mod1.HelloWorld())
+}
